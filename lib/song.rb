da2d1ad08887cb769f3a563843e29d15aa49371f
Heshie Brody <4hbrody@gmail.com>
2018-08-26 03:46:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
     song.artist_name = artist
     song
   end
-  def self.new_from_filename(filename)
+  def self.create_from_filename(filename)
     song self.new_from_filename(filename)
     self.all << song
   end
